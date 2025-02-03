417ed117e3e7cd9c84f89890debd70141e273baa
Anas P <anasdtp21@gmail.com>
2025-02-03 20:44:41 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import pickle
+import re
+# from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+# import nltk
+
+
+app = Flask(__name__)
+
+# Load the model and vectorizer
+with open('fake_news_model.pkl', 'rb') as f:
+    model, tfidf = pickle.load(f)
+
+
+def preprocess_text(text):
+    text = re.sub(r'[^\w\s]', '', text.lower())
+    # tokens = word_tokenize(text)  # Remove this line
+    tokens = text.split()  # Use Python's split() instead
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens if token not in stop_words]
+    return " ".join(tokens)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        user_input = request.form["news_text"]
+        if user_input:
+            processed_input = preprocess_text(user_input)
+            vectorized_input = tfidf.transform([processed_input])
+            prediction = model.predict(vectorized_input)[0]
+
+    return render_template("index.html", prediction=prediction)  # Render the HTML template
+
+if __name__ == "__main__":
+    app.run(debug=True)  # debug=True for development; set to False in production
